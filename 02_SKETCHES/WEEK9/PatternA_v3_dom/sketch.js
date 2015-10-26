8a10ec7d7d743f34ad010a3bef2759086411b685
evejweinberg <evejweinberg@gmail.com>
2015-10-25 20:17:47 -0400

    lots

--- 02_SKETCHES/WEEK9/PatternA_v3_dom/sketch.js
+++ 02_SKETCHES/WEEK9/PatternA_v3_dom/sketch.js
@@ -3,6 +3,7 @@
 var sprites = []; //all jitter objects go in here
 var spriteLibrary = []; //png sequence
 var abgAll = [];
+var abgAllDom = [];
 var aFills = []; //png sequence
 var spriteAssets = [];
 var frameNumber = 0;
@@ -12,36 +13,37 @@ var sprite;
 var aFill, aLines;
 var globalCounter = 0;
 var transition = false;
-var roveBothax, roveBothay, roveBothbx, roveBothby;
+var roveBothax, roveBothay, roveBothbx, roveBothby, wiggleaway;
 
-function preload() {//load all images here
+function preload() { //load all images here
   aLines = loadImage("assets/alines.png");
   for (var i = 0; i < 3; i++) {
     aFill = "assets/Afill_000" + i + ".png"; //store the name of the images
-     aFills.push(loadImage(aFill)); //push images into this array
-    
-  }
-  
+    aFills.push(loadImage(aFill)); //push images into this array
 
-  for (var i = 0; i < 3; i++) {
+  }
+
+
+  for (var i = 0; i < 7; i++) {
     abg = "assets/Abg_000" + i + ".png"; //store the name of the images
-    abgAll.push(loadImage(abg)); //push images into this array
-   
+    abgAllDom.push(createImg(abg)); //push into DOM elements
+
   }
 
-  for (var i = 0; i < sprite1Total; i++) {//load all the image names
+  for (var i = 0; i < sprite1Total; i++) { //load all the image names
 
-    if (i < 10) {//for 1 digit ones, add the zero
+    if (i < 10) { //for 1 digit ones, add the zero
       sprite = "assets/sprite1_0" + i + ".png";
-    } else {//for 2 digit ones dont
+    } else { //for 2 digit ones dont
       sprite = "assets/sprite1_" + i + ".png";
     }
-    spriteLibrary.push(loadImage(sprite));//push them all into an array
+    spriteLibrary.push(loadImage(sprite)); //push them all into an array
     //these will be called inside the jitter object
   }
 }
 
 function setup() {
+
   var fp = createDiv('');
   var fpimg = createImg("assets/fp_00.png");
   fpimg.parent(fp).size(60, 90);
@@ -51,6 +53,16 @@ function setup() {
     sprites.push(new Jitter());
     //push 50 jitters, each one with animations looping inside
   }
+
+// abgAllDom[5].position(0,0).mouseOver(wiggle);
+
+
+}//////SETUP ENDS////
+
+function wiggle(){
+  console.log("wiggle!!")
+  wiggleaway = random(-5,5);
+  //scoot in a stop-motion way, away from mouse
 }
 
 function BreakOut() {
@@ -58,21 +70,28 @@ function BreakOut() {
 }
 
 function draw() {
-  globalCounter = globalCounter + .4;
-  
-  if (transition === true) {
-  roveBothax = cos(millis() / 10) * 7;
-  roveBothay = sin(millis() / 10) * 7;
-  roveBothbx = sin(millis() / 10) * 7;
-  roveBothby = cos(millis() / 10) * 7;
-  }
-    if (transition === false) {
-  roveBothax = cos(millis() / 200) * 5;
-  roveBothay = sin(millis() / 200) * 5;
-  roveBothbx = sin(millis() / 180) * 3;
-  roveBothby = cos(millis() / 160) * 3;
-  }
   background(255, 0, 0);
+bgCollage(0+wiggleaway,0+wiggleaway);
+
+  globalCounter = globalCounter + .4;
+
+  if (transition === true) {
+     
+    roveBothax = cos(millis() / 10) * 7;
+    roveBothay = sin(millis() / 10) * 7;
+    roveBothbx = sin(millis() / 10) * 7;
+    roveBothby = cos(millis() / 10) * 7;
+   
+  }
+  if (transition === false) {
+   
+    roveBothax = cos(millis() / 200) * 5;
+    roveBothay = sin(millis() / 200) * 5;
+    roveBothbx = sin(millis() / 180) * 3;
+    roveBothby = cos(millis() / 160) * 3;
+  }
+
+  
 
 
 
@@ -83,26 +102,38 @@ function draw() {
   for (var i = 0; i < sprites.length; i++) {
     sprites[i].display();
   }
-  
+
   image(aFills[floor(globalCounter % 3)], 35 + roveBothax, 150 + roveBothay);
   image(aLines, 30 + roveBothbx, 150 + roveBothby);
 
 
 }
 
-function bgCollage(){
-  this.x = x;
-  this.y = y;
-  for (var i=0;i<7;i++){
-  image(abgAll[i],40,40);
+function bgCollage(x,y) {
+this.x = x;
+this.y = y;
+this.rove = sin(millis())*6;
+
+// abgAllDom[0].position(200, 300).mouseOver(wiggle);
+
+
+  if (transition === true) {
+   abgAllDom[0].position(200+wiggleaway, 300+wiggleaway).mouseOver(wiggle);
+     abgAllDom[1].position(50+wiggleaway, 100+wiggleaway).mouseOver(wiggle);
+      abgAllDom[2].position(0+wiggleaway, 500+wiggleaway).mouseOver(wiggle);
+       abgAllDom[3].position(100+wiggleaway, 200+wiggleaway).mouseOver(wiggle);
+
   }
-  
-    if (transition === true) {
-    }
-    
-      if (transition === false) {
-    }
-  
+
+  if (transition === false) {
+    abgAllDom[0].position(200+wiggleaway, 300+wiggleaway).mouseOver(wiggle);
+     abgAllDom[1].position(50+wiggleaway, 100+wiggleaway).mouseOver(wiggle);
+      abgAllDom[2].position(0+wiggleaway, 500+wiggleaway).mouseOver(wiggle);
+       abgAllDom[3].position(100+wiggleaway, 200+wiggleaway).mouseOver(wiggle);
+ 
+
+  }
+
 }
 
 
@@ -121,7 +152,7 @@ function Jitter() {
   this.display = function() {
 
     if (transition === true) {
-      
+
       this.scaleup = cos(millis() / 100) * 40;
       this.rw = this.rh + this.scaleup;
       this.rovex = cos(millis() / 50) * this.speed;
@@ -129,10 +160,10 @@ function Jitter() {
       this.x = this.x + this.rovex;
       this.y = this.y + this.rovey;
 
-     
 
-    } 
-    if (transition === false){
+
+    }
+    if (transition === false) {
 
       this.scaleup = cos(millis() / 500) * 20;
       this.rw = this.rh + this.scaleup;
@@ -140,16 +171,16 @@ function Jitter() {
       this.rovey = sin(millis() / 500) * this.speed;
       this.x = this.x + this.rovex;
       this.y = this.y + this.rovey;
-      
 
-      
+
+
     }
     image(spriteLibrary[floor(this.startingFrame)], this.x, this.y, this.rw, this.rw);
-      this.startingFrame = this.startingFrame + .5;
-    
+    this.startingFrame = this.startingFrame + .5;
+
     if (this.startingFrame == sprite1Total) {
-        this.startingFrame = 0;
-      }
+      this.startingFrame = 0;
+    }
   }
 
 
